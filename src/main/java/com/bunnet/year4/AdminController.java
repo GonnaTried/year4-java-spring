4a325a58f4d5862d90c5af791ab07ce5a8890984
BePlay <kornbunnet@gmail.com>
2025-06-24 19:17:00 +0700

    admin controller

---
+++ src/main/java/com/bunnet/year4/AdminController.java
@@ -0,0 +1,19 @@
+package com.bunnet.year4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/index")
+    public String redirectToIndex() {
+        return "forward:/Admin/index.html";
+    }
+
+    @GetMapping("/cv")
+    public String redirectToCV() {
+        return "forward:/Admin/cv.html";
+    }
+
+}
